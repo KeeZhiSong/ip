972cef658c07920e5ea9308c6573e52f036d6a73
kee xin hui <keezhisongkee@gmail.com>
2026-01-23 10:12:20 +0800

    Add automated text UI testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,18 +13,24 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin \
+    ../src/main/java/Task.java \
+    ../src/main/java/Todo.java \
+    ../src/main/java/Deadline.java \
+    ../src/main/java/Event.java \
+    ../src/main/java/SigmaWolfException.java \
+    ../src/main/java/SigmaWolf.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin SigmaWolf < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT 2>/dev/null
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
